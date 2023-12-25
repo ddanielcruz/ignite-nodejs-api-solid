4384e6a72ee8e1d0f7412c14dbdfde5588b39d5d
Daniel Cruz <danielcunha54@gmail.com>
2023-12-25 07:34:30 -0300

    Add ESLint

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,6 @@
 import { app } from './app'
+import { env } from './env'
 
-app
-  .listen({
-    host: '0.0.0.0',
-    port: 3000
-  })
-  .then(address => {
-    console.log(`🚀 Server listening on ${address}`)
-  })
+app.listen({ host: '0.0.0.0', port: env.PORT }).then((address) => {
+  console.log(`🚀 Server listening on ${address}`)
+})
